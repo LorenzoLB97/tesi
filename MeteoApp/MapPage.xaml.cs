5f6440eabd25550874ed851120220bf406d576ab
LorenzoLB97 <lorenzolobrutto@gmail.com>
2024-10-30 19:37:32 +0100

    refactory del metodo OnMapClicked

--- MeteoApp/MapPage.xaml.cs
+++ MeteoApp/MapPage.xaml.cs
@@ -8,8 +8,6 @@ namespace MeteoApp;
 
 public partial class MapPage : ContentPage
 {
-    private readonly GeoLocationService _geoLocationService;
-
     public MapPage()
     {
         InitializeComponent();
@@ -24,33 +22,6 @@ public partial class MapPage : ContentPage
 
     async void OnMapClicked(object sender, MapClickedEventArgs e)
     {
-        var placemarks = await Geocoding.GetPlacemarksAsync(e.Location.Latitude, e.Location.Longitude);
-        var placemark = placemarks?.FirstOrDefault();
-
-        if (placemark != null)
-        {
-            string completeAddress = $"{placemark.Thoroughfare}, {placemark.Locality}, {placemark.PostalCode}, {placemark.CountryName}";
-            string street = placemark.Thoroughfare;
-            string city = placemark.Locality;
-            string postalCode = placemark.PostalCode;
-            string country = placemark.CountryName;
-
-            // Mostra l'alert con l'indirizzo completo
-            await DisplayAlert("Indirizzo Selezionato", $"{completeAddress}", "OK");
-
-            App.Database.SaveEntry(new Entry()
-            {
-                CompleteAddress = completeAddress,
-                Street = street,
-                City = city,
-                PostalCode = postalCode,
-                Country = country
-            });
-        }
-        else
-        {
-            // Mostra un messaggio di errore se l'indirizzo non è disponibile
-            await DisplayAlert("Errore", "Non è stato possibile determinare l'indirizzo.", "OK");
-        }        
+        App.Database.SaveEntry(await GeoLocationService.ReverseGeoCoding(e.Location));        
     }
 }
