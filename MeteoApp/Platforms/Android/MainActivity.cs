bd66c8d09e9dec001b290eab135957afa0512eba
LorenzoLB97 <lorenzolobrutto@gmail.com>
2024-11-19 16:22:03 +0100

    finalmente compila e lancia con il plugin firebase

--- MeteoApp/Platforms/Android/MainActivity.cs
+++ MeteoApp/Platforms/Android/MainActivity.cs
@@ -1,11 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Runtime;
 using Android.OS;
+using Firebase;
 
-namespace MeteoApp;
-
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity
+namespace MeteoApp
 {
-}
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
 
+            // Usa semplicemente "this" come contesto
+            FirebaseApp.InitializeApp(this);
+        }
+    }
+}
