df643971d203e9879807303816bd7ff82bf02756
LorenzoLB97 <lorenzolobrutto@gmail.com>
2024-12-19 14:46:18 +0100

    fix

--- MeteoApp/App.xaml.cs
+++ MeteoApp/App.xaml.cs
@@ -39,7 +39,7 @@ public partial class App : Application
 
         reference.GetCurrentLocation();
     }
-
+    
     private void LoadDBEntries()
     {
         MeteoListPage reference = MainPage as MeteoListPage;
